11d34690200d029bdca609bf156d5d861f6c16e1
Saturna19 <saturna19.stream@outlook.fr>
2024-04-07 01:02:16 +0200

    Update questions.py
    Ajout de askQuestion, Questions et ProductsRelative pour définir quels sont les types de 'produits' utilisés.

--- questions.py
+++ questions.py
@@ -1,5 +1,31 @@
 from utils.data import extrait_donnees
+from random import randint
+
 
 data = extrait_donnees('data/drugs.csv', separator=',', is_in_quote=True)
 
-print(data)
+ProductsRelative = {
+    "Alcool": ["Totals.Alcohol.Use Disorder Past Year.12-17", "Totals.Alcohol.Use Disorder Past Year.18-25", "Totals.Alcohol.Use Disorder Past Year.26+"]
+}
+
+Questions = {
+    'drogue': [
+        {"q": "En quelle année le taux de {illicitProduct} a été atteint dans l'êtat de/du (l') {state} ?", "type": "illicitProduct", "answers": "nbGuess", "proposedAnswers": 3}
+    ]
+}
+
+def askQuestion(category, questionNumber):
+    SelectedQuestion = Questions[category][questionNumber]
+
+    if SelectedQuestion["answers"] == "nbGuess":
+        print('nbGuess.')
+
+
+
+    print(SelectedQuestion["q"].format(year = data[5]["Year"], state = data[5]["State"], bababo = "test", illicitProduct = "Alcool"))
+
+
+askQuestion('drogue', 0)
+
+
+# print(data)
